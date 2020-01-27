3e71665ab7e14198e5a8e45efacf35e73e91b6da
Max Dauber <max@dauberfamily.com>
2020-01-27 14:02:34 +0100

    changed gravatar link

--- _plugins/gravatar.rb
+++ _plugins/gravatar.rb
@@ -8,7 +8,8 @@ module Jekyll
 		def gravatar(input)
 			email_address = input.downcase.strip
 			hash = Digest::MD5.hexdigest(email_address)
-			"https://www.gravatar.com/avatar/#{hash}"
+			#"https://www.gravatar.com/avatar/#{hash}"
+			"https://www.gravatar.com/avatar/243a658fd5860f7cc2b21e501b21472a"
 		end
 	end
 end
